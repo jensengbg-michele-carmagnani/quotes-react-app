bdf479867d95911e12585c7cb837044b35b6305a
Michele Carmagnani <michele.carmagnani@gmail.com>
2021-09-29 13:39:53 +0200

    add lazy mode

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
-import AllQuotes from "./Page/AllQuotes";
-import DetailQuote from "./Page/DetilQuote";
-import NewQuote from "./Page/NewQuote";
 import Layout from "./components/layout/Layout";
-import NotFouond from "./Page/NotFouond";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+const AllQuotes= React.lazy(()=> import("./Page/AllQuotes"));
+const DetailQuote = React.lazy(() => import("./Page/DetilQuote")) ;
+const NotFouond = React.lazy(() => import("./Page/NotFouond"));
+const NewQuote = React.lazy(() => import("./Page/NewQuote"));
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/allquotes" />
-        </Route>
-        <Route path="/allquotes" exact>
-          <AllQuotes />
-        </Route>
-        <Route path="/quote/:quoteId">
-          <DetailQuote />
-        </Route>
-        <Route path="/newquote">
-          <NewQuote />
-        </Route>
-        <Route path="*">
-          <NotFouond />
-        </Route>
-      </Switch>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/allquotes" />
+          </Route>
+          <Route path="/allquotes" exact>
+            <AllQuotes />
+          </Route>
+          <Route path="/quote/:quoteId">
+            <DetailQuote />
+          </Route>
+          <Route path="/newquote">
+            <NewQuote />
+          </Route>
+          <Route path="*">
+            <NotFouond />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
